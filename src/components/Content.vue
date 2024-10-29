3ac36caccb1b111b59599ce38f8334602eee17e1
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-29 18:57:16 -0500

    added create action

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,10 +1,12 @@
 <script>
 import GuitarsIndex from "./GuitarsIndex.vue"
 import axios from 'axios'
+import GuitarsNew from './GuitarsNew.vue'
 
 export default { 
   components: { 
     GuitarsIndex,
+    GuitarsNew,
   },
 
   data: function () { 
@@ -23,16 +25,27 @@ export default {
         this.guitars = response.data;
       });
     },
+    handleCreateGuitar: function (params) { 
+      axios
+      .post('http://localhost:3000/guitars.json', params).then((response) => { 
+        console.log("Guitars Create", response);
+        this.photos.push(response.data);
+      })
+      .catch((error) => { 
+        console.log("guitars create error", error.response)
+      })
+    },
   },
 };
-
 
 </script>
 
 <template>
   <main>
     <h1>Welcome to Vue!</h1>
+    <GuitarsNew v-on:createGuitar="handleCreateGuitar"/>
     <GuitarsIndex v-bind:guitars="guitars"/>
+    
   </main>
 </template>
 
---
+++ src/components/GuitarsNew.vue
@@ -0,0 +1,38 @@
+<script>
+
+export default { 
+  data: function () { 
+    return { 
+      newGuitarParams: {},
+    };
+  },
+  methods: { 
+    handleSubmit: function () { 
+      this.$emit('createGuitar', this.newGuitarParams);
+      this.newGuitarParams = {};
+    },
+  },
+};
+
+</script>
+
+<template>
+  <div>
+    <h1>New Guitar</h1>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        name: <input type="text" v-model="newGuitarParams.name" />
+      </div>
+      <div>
+        Price: <input type="text" v-model="newGuitarParams.price"/>
+      </div>
+      <div>
+        Year: <input type="text" v-model="newGuitarParams.year" />
+      </div>
+      <div>
+        Image_URL: <input type="text" v-model="newGuitarParams.image"/>
+      </div>
+      <button type='submit'>Create</button>
+    </form>
+  </div>
+</template>
