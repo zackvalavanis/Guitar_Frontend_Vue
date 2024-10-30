9f79ccfa88125af53b2cc512e334e256b09999ed
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-29 19:47:02 -0500

    added delete and update functions

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -50,6 +50,32 @@ export default {
     handleClose: function () { 
       this.isCurrentGuitarVisible = false
     },
+    handelUpdateGuitar: function (id, params) { 
+      console.log('handleUpdateGuitar', id, params);
+      axios
+        .patch(`http://localhost:3000/guitars/${id}.json`, params).then((response) => { 
+          console.log("guitars update", response);
+          this.guiitars = this.guitars.map((guitar) => { 
+            if (guitar.id === response.data.id){ 
+              return response.data;
+            } else { 
+              return guitar;
+            }
+          });
+          this.handleClose;
+        })
+        .catch((error) => { 
+          console.log("guitar update error", error.response);
+        });
+    },
+    handleDestroyGuitar: function (guitar) { 
+      axios.delete(`http://localhost:3000/guitars/${guitar.id}.json`).then((response) => { 
+        console.log("guitar Destroy", response);
+        var index = this.guitars.indexOf(guitar);
+        this.guitar.splice(index, 1);
+        this.handleClose();
+      });
+    },
   },
 };
 
@@ -61,7 +87,7 @@ export default {
     <GuitarsNew v-on:createGuitar="handleCreateGuitar"/>
     <GuitarsIndex v-bind:guitars="guitars" v-on:showGuitar="handleShowGuitar"/>
     <Modal v-bind:show="isCurrentGuitarVisible" v-on:close="handleClose">
-      <GuitarsShow v-bind:guitar="currentGuitar"/>
+      <GuitarsShow v-bind:guitar="currentGuitar" v-on:updateGuitar="handelUpdateGuitar" v-on:destroyGuitar="handleDestroyGuitar"/>
     </Modal>
   </main>
 </template>
--- src/components/GuitarsShow.vue
+++ src/components/GuitarsShow.vue
@@ -3,6 +3,19 @@ export default {
   props: { 
     guitar: Object,
   },
+  data: function () { 
+    return { 
+      editGuitarParams: {},
+    };
+  },
+  created: function () {
+    this.editGuitarParams = { ...this.guitar };
+  },
+  methods: { 
+    handleSubmit: function () { 
+      this.$emit("updateGuitar", this.guitar.id, this.editGuitarParams)
+    },
+  },
 };
 </script>
 
@@ -12,6 +25,22 @@ export default {
     <p>Name: {{ guitar.name }}</p>
     <p>Year: {{ guitar.year }}</p>
     <p>Price: {{ guitar.price }}</p>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        Name: <input type="text" v-model="editGuitarParams.name"/>
+      </div>
+      <div>
+        Year: <input type="text" v-model="editGuitarParams.year" />
+      </div>
+      <div>
+        Price: <input type="text" v-model="editGuitarParams.price" />
+      </div>
+      <div>
+        Image_URL: <input type="tect" v-model="editGuitarParams.image" />
+      </div>
+      <button type="submit">Update Guitar</button>
+      <button v-on:click="$emit('destroyGuitar', guitar)">Delete Guitar</button>
+    </form>
   </div>
 </template>
 
